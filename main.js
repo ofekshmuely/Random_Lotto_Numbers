66523f26b3fddbac013d6d0025a5e0ec3bc30640
Ofek <32801785+ofekshmuely@users.noreply.github.com>
2018-05-28 12:28:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+//random numbers shoot
+n=6;
+array=Array.from({length:37},(v,k)=>k+1);
+var goRandom = array.sort(function(){return .6 - Math.random()});
+
+var goRandom=goRandom.slice(0,n);
+
+goRandom = goRandom.join(" ");
+
+
+$('#out').html(goRandom.toString() );
+
+
+
