e1c20d0e8e4b7a1d103088e15d512e94d7c3a978
Ofek Shmuely <meshuba.global@gmail.com>
2022-10-06 15:41:20 +0300

    update

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 //random numbers shoot
-n=6;
-array=Array.from({length:37},(v,k)=>k+1);
-var goRandom = array.sort(function(){return .6 - Math.random()});
 
-var goRandom=goRandom.slice(0,n);
+function myFunction() {
+  let displayNumbers = 6;
+  let array = Array.from({ length: 37 }, (v, k) => k + 1);
+  let goRandom = array.sort(function () {
+    return 0.6 - Math.random();
+  });
 
-goRandom = goRandom.join(" ");
+  goRandom = goRandom.slice(0, displayNumbers);
 
+  goRandom = goRandom.join(" | ");
 
-$('#out').html(goRandom.toString() );
+  $("#out").html(goRandom.toString());
+
+  document.getElementById("goStrong").innerHTML = Math.floor(
+    Math.random() * 7 + 1
+  );
+}
